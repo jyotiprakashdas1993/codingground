fe70d2a04b62ad0e0386d4787ed34eeefa5d180d
jyotiprakashdas1993 <das.jyopra@gmail.com>
2017-07-06 22:41:40 +0530

    Created by Coding Ground Client

---
+++ MyFirstProj/AddVariable.java
@@ -0,0 +1,19 @@
+public class AddVariable{
+
+     public static void main(String []args){
+         int x = 10;
+         int y = 13;
+         int z = x+y;
+        System.out.println(z);
+        
+        float x1 = 10;
+        float y1 = 13;
+        float z1 = x+y;
+        System.out.println(z1);
+        
+        double x2 = 10.0;
+        double y2 = 13.0;
+        double z2 = x+y;
+        System.out.println(z2);
+     }
+}
